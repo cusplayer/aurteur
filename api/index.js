ffbb71428adb1f702111815e423285f22a558013
cusplayer <55591964+cusplayer@users.noreply.github.com>
2024-05-29 18:31:47 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI || 'https://aurteur.com/api/callback';
 
 let accessToken = null;
-let userAccessToken = null;
+let userAccessToken = process.env.userAccessToken;
 let refreshToken = null;
 let accessTokenExpiresAt = null;
 let currentTrackId = null;
