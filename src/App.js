f070e285cc6106bb9b0472243b1303a670f83fb5
cusplayer <55591964+cusplayer@users.noreply.github.com>
2024-05-31 13:10:34 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,27 @@ initArrowNavigation();
 function App() {
   const [trackInfo, setTrackInfo] = useState({});
   const [isPlaying, setIsPlaying] = useState({});
-
+  
   useEffect(() => {
     let isMounted = true; // Set isMounted to true here
-    
+
+    async function fetchInitialTrackInfo() {
+      try {
+        const response = await axios.get('/api/current-track');
+        if (response.data) {
+          const { name, album, artist, is_playing } = response.data;
+          setIsPlaying(is_playing);
+          if (is_playing) {
+            setTrackInfo({ name, album, artist });
+          } else {
+            setTrackInfo({});
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching initial track:', error.response?.data || error.message);
+      }
+    }
+
     async function fetchTrackInfo() {
       if (!isMounted) {
         return;
@@ -46,32 +63,15 @@ function App() {
         }
       }
     }
+  
+    fetchInitialTrackInfo();
+    fetchTrackInfo();
+  
     return () => {
       isMounted = false; // Set isMounted to false when the component is unmounted
     };
   }, []);
-  
-  useEffect(() => {
-    async function fetchInitialTrackInfo() {
-      try {
-        const response = await axios.get('/api/current-track');
-        if (response.data) {
-          const { name, album, artist, is_playing } = response.data;
-          setIsPlaying(is_playing);
-          if (is_playing) {
-            setTrackInfo({ name, album, artist });
-          } else {
-            setTrackInfo({});
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching initial track:', error.response?.data || error.message);
-      }
-    }
 
-    fetchInitialTrackInfo();
-    fetchTrackInfo();
-  }, []);
 
   return (
     <div className="main-page">
