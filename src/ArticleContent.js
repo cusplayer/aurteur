0c30fc72767ef11f8c89250b395de7a722366a68
cusplayer <55591964+cusplayer@users.noreply.github.com>
2024-05-31 14:20:40 +0300

    Skeleton Loader

--- src/ArticleContent.js
+++ src/ArticleContent.js
@@ -4,19 +4,23 @@ import axios from 'axios';
 import Markdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw'
 import moment from 'moment';
+import Skeleton from 'react-loading-skeleton';
 
 const ArticleContent = ({ fileName }) => {
   const [metadata, setMetadata] = useState(null);
   const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get(`api/articles/${fileName}`)
+    axios.get(`http://localhost:5000/api/articles/${fileName}`)
       .then(response => {
         setMetadata(response.data.metadata);
         setContent(response.data.content);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching article content:', error);
+        setLoading(false);
       });
   }, [fileName]);
 
@@ -24,16 +28,20 @@ const ArticleContent = ({ fileName }) => {
     <div className="article-box">
       <h1>{fileName.split('.').slice(0, -1).join('.')}</h1>
       <Markdown>---</Markdown>
-      {metadata && (
-        <div className="article-metadata">
-          <h1>{metadata.title}</h1>
-          <p>Folder: {metadata.folder}</p>
-          <p>Tags: {metadata.tags.map(tag => `#${tag}`).join(' ')}</p>
-          <p>Date: {moment(metadata.date).format('DD/MM/YYYY')}</p>
-        </div>
+      {loading ? (
+        <Skeleton count={5} />
+      ) : (
+        metadata && (
+          <div className="article-metadata">
+            <h1>{metadata.title}</h1>
+            <p>Folder: {metadata.folder}</p>
+            <p>Tags: {metadata.tags.map(tag => `#${tag}`).join(' ')}</p>
+            <p>Date: {moment(metadata.date).format('DD/MM/YYYY')}</p>
+          </div>
+        )
       )}
       <div className="article-content">
-        <Markdown rehypePlugins={[rehypeRaw]}>{content}</Markdown>
+        {loading ? <Skeleton count={10} /> : <Markdown rehypePlugins={[rehypeRaw]}>{content}</Markdown>}
       </div>
     </div>
   );
