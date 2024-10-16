73f7d783f19f88b56ad64c9a6f923733e7932cc9
cusplayer <gcosplayer@gmail.com>
2024-10-16 19:34:17 +0300

    Finally API routing fixed

--- webpack.config.js
+++ webpack.config.js
@@ -4,80 +4,89 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv');
 
-const env = Dotenv.config({ path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`) }).parsed;
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const currentEnv = mode;
+  const envFilePath = path.resolve(__dirname, `.env.${currentEnv}`);
+  const envVariables = Dotenv.config({ path: envFilePath }).parsed || {};
 
-module.exports = {
-  entry: './src/index.tsx',
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    alias: {
-      'components': path.resolve(__dirname, 'src/components'),
-      'api': path.resolve(__dirname, 'src/api'),
+  const envKeys = Object.keys(envVariables).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(envVariables[next]);
+    return prev;
+  }, {});
+
+  return {
+    mode: mode,
+    entry: './src/index.tsx',
+    target: 'web',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      publicPath: '/',
     },
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              icon: true,
-              exportType: 'named',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        api: path.resolve(__dirname, 'src/api'),
+      },
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                icon: true,
+                exportType: 'named',
+              },
             },
-          },
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
+          ],
+          exclude: /node_modules/,
         },
-        exclude: /node_modules/,
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+          },
+          exclude: /node_modules/,
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              }
             }
-          }
-        ]
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(env)
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+          ]
+        },
+      ],
     },
-    compress: true,
-    port: 3000,
-  },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new webpack.DefinePlugin(envKeys),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000,
+    },
+  };
 };
