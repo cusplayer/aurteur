2319439f170b5c0aa11c2268d4e86f7f1867a4fd
cusplayer <55591964+cusplayer@users.noreply.github.com>
2024-12-09 17:09:30 +0300

    T to t

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,7 +1,7 @@
 import { Text } from '../types/types';
 
 export const getTextsData = async (): Promise<Text[]> => {
-  const response = await fetch('/texts/Texts.json');
+  const response = await fetch('/texts/texts.json');
   const data = await response.json();
   return data.texts;
 };
